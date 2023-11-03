75e87fea0624f2a31e7d6a02f697c35d4eb130ca
Tmark93 <129175598+Tmark93@users.noreply.github.com>
2023-11-03 12:50:27 +0100

    l

--- Notes/app/src/main/java/com/example/notes/Notes.kt
+++ Notes/app/src/main/java/com/example/notes/Notes.kt
@@ -1,9 +1,15 @@
 package com.example.notes
 
+<<<<<<< HEAD
 import java.util.UUID
 
 data class Notes (
     val title: String,
     var text: String,
     val id: UUID = UUID.randomUUID()
+=======
+data class Notes (
+    val title: String,
+    var text: String
+>>>>>>> 1679fcc (Data Class notes)
 )
