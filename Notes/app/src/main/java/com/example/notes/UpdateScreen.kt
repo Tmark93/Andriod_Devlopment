321cce039721560179d47f21fda985b62306dc87
Tmark93 <129175598+Tmark93@users.noreply.github.com>
2023-11-03 12:53:30 +0100

    l

--- Notes/app/src/main/java/com/example/notes/UpdateScreen.kt
+++ Notes/app/src/main/java/com/example/notes/UpdateScreen.kt
@@ -1,2 +1,74 @@
 package com.example.notes
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+
+
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview(showBackground = true)
+fun UpdateScreen() {
+    var newNote by remember { mutableStateOf(0) }
+
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    TextInputTopBar()
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.ArrowBack, "backCcon")
+                    }
+                }
+            )
+        }
+    ){
+        Text("Number of notes: $newNote", modifier = Modifier.fillMaxSize(), textAlign = TextAlign.Center)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextInputTopBar(){
+    var text by rememberSaveable {
+        mutableStateOf("")
+    }
+    TextField(value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = "Enter Label") },
+        placeholder = { Text(text = "PlaceHolder")},
+    )
+}
