4ea61368625924172b06b57742d8a793a4f31ea3
Tmark93 <129175598+Tmark93@users.noreply.github.com>
2023-11-03 12:42:35 +0100

    l

--- Notes app/app/build.gradle.kts
+++ Notes app/app/build.gradle.kts
@@ -4,17 +4,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.notes"
-    compileSdk = 33
+    namespace = "com.example.notesapp"
     compileSdk = 34
 
-
     defaultConfig {
-        applicationId = "com.example.notes"
+        applicationId = "com.example.notesapp"
         minSdk = 28
         targetSdk = 34
-        targetSdk = 33
-
         versionCode = 1
         versionName = "1.0"
 
@@ -27,10 +23,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
@@ -55,17 +48,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation ("androidx.compose.material:material-icons-extended:1.5.4")
-    implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
