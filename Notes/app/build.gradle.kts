272b8e67d5dda4a9dc250076b46c1c58756b21ad
Tmark93 <129175598+Tmark93@users.noreply.github.com>
2023-11-03 12:51:54 +0100

    l

--- Notes/app/build.gradle.kts
+++ Notes/app/build.gradle.kts
@@ -6,15 +6,11 @@ plugins {
 android {
     namespace = "com.example.notes"
     compileSdk = 33
-    compileSdk = 34
-
 
     defaultConfig {
         applicationId = "com.example.notes"
         minSdk = 28
-        targetSdk = 34
         targetSdk = 33
-
         versionCode = 1
         versionName = "1.0"
 
@@ -55,7 +51,6 @@ android {
 
 dependencies {
 
-    implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
@@ -64,8 +59,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation ("androidx.compose.material:material-icons-extended:1.5.4")
-    implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
